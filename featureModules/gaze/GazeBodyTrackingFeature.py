2456b1d721a6e461a817aaef71507bb895fb997b
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-07-16 14:33:01 -0600

    change origin

--- featureModules/gaze/GazeBodyTrackingFeature.py
+++ featureModules/gaze/GazeBodyTrackingFeature.py
@@ -8,8 +8,7 @@ class GazeBodyTrackingFeature(IFeature):
         self.shift = shift
 
     def world_to_camera_coords(self, r_w, rotation, translation):
-        r_w = r_w[:, None]
-        return (np.dot(rotation, r_w) + translation)[:,0]
+        return np.dot(rotation, r_w) + translation
 
     def get_joint(self, joint, body, rotation, translation):
         r_w = np.array(body["joint_positions"][joint.value])
@@ -21,15 +20,14 @@ class GazeBodyTrackingFeature(IFeature):
             #     pos = convert2D(b["joint_positions"][i.value], rotation, translation, cameraMatrix, dist)
             #     cv.circle(frame, pos.astype(int), 5, (255,255,255), -1)
             nose = self.get_joint(Joint.NOSE, b, rotation, translation)
+            head = self.get_joint(Joint.HEAD, b, rotation, translation)
 
             ear_left = self.get_joint(Joint.EAR_LEFT, b, rotation, translation)
             ear_right = self.get_joint(Joint.EAR_RIGHT, b, rotation, translation)
+            ear_center = (ear_left + ear_right) / 2
 
             eye_left = self.get_joint(Joint.EYE_LEFT, b, rotation, translation)
             eye_right = self.get_joint(Joint.EYE_RIGHT, b, rotation, translation)
-
-            ear_center = (ear_left + ear_right) / 2
-
 
             dir = nose - ear_center
             dir /= np.linalg.norm(nose - ear_center)
@@ -38,12 +36,12 @@ class GazeBodyTrackingFeature(IFeature):
 
             p1_3d = origin
             p2_3d = origin + 1000*dir
-            p1 = convert2D(p1_3d, cameraMatrix, dist)
-            p2 = convert2D(p2_3d, cameraMatrix, dist)
-
-            cv.line(frame, p1.astype(int), p2.astype(int), (255,255,0), 2)
 
             cone = ConeShape(p1_3d, p2_3d, 80, 100, cameraMatrix, dist)
-            cone.projectRadiusLines(self.shift, frame, True, True, True)
+            cone.projectRadiusLines(self.shift, frame, False, False, True)
+
+            p1 = convert2D(p1_3d, cameraMatrix, dist)
+            p2 = convert2D(p2_3d, cameraMatrix, dist)
+            cv.line(frame, p1.astype(int), p2.astype(int), (255,255,0), 2)
             
             checkBlocks(blocks, blockStatus, cameraMatrix, dist, depth, cone, frame, self.shift, True)
