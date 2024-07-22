8bfeb7032127f50c822f9ab9dcfcf23e91cee6ba
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-07-22 16:34:33 -0600

    update test script

--- test_prop_extract.py
+++ test_prop_extract.py
@@ -6,6 +6,11 @@ model, tokenizer = load_model(model_dir)
 
 while True:
     text = input("enter phrase: ")
-    output = process_sentence(text, model, tokenizer, verbose=False)
-    print("prop: ", output)
+    colors = ["red", "blue", "green", "purple", "yellow"]
+    numbers = ["10", "20", "30", "40", "50"]
+    if not any(i in text for i in colors + numbers):
+        prop, num_filtered_props = "no prop", 0
+    else:
+        prop, num_filtered_props = process_sentence(text, model, tokenizer, verbose=False)
+    print("prop: ", prop)
     
