23b7f206556a9c0e0b2dce69a077b432dc39507d
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-07-23 11:14:47 -0600

    remove file

--- featureModules/asr/AsrFeatureEval.py
+++
@@ -1,9 +0,0 @@
-from featureModules import AsrFeature
-
-class AsrFeatureEval(AsrFeature):
-    def __init__(self, input_dir=None, log_dir=None):
-        pass
-
-    def processFrame(self, frame, frame_count):
-        pass
-
