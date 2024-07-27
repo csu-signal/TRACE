8bc81d81820f0035528495b29f65972aa5daaf67
Bhalla <brady03@colostate.edu>
2024-07-27 00:03:34 -0600

    move into a folder and make functions so a script can generate all inputs

---
+++ wtd_annotations/postProcessGTConversion_gesture.py
@@ -0,0 +1,59 @@
+import argparse
+from utils import GamrCategory, GamrTarget, convertGamrValues, loadGamrFeatures, loadKeyFrameFeatures
+import os
+import csv
+
+def initalizeCsv(path):
+    if os.path.exists(path):
+        os.remove(path)
+
+    with open(path, 'a', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["frame", "blocks", "body_id", "handedness"])
+    return 
+
+def LogCsv(path, frame, blocks, body_id, handedness):
+    with open(path, 'a', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([frame, blocks, body_id, handedness])
+    return 
+
+def create_gesture_input(gamrPath, annotationsFile, outputFile):
+    initalizeCsv(outputFile)
+
+    pointAnnotations = [annotationsFile]
+    pointFeatures = loadKeyFrameFeatures(pointAnnotations)
+    gamrFeatures = loadGamrFeatures(gamrPath)
+
+    #for each ground truth pointing frame find the ground truth gamrs and log the targets
+    for f in pointFeatures:
+        start = f[4]
+        end = f[5]
+        for frameCount in range(int(start), int(end), 1):
+            totalSeconds = frameCount / 30 #30 frames per second
+            gamrs = convertGamrValues(totalSeconds, gamrFeatures)
+
+            if(gamrs):
+                blocks = []
+                for g in gamrs:
+                    if(g.category == GamrCategory.DEIXIS):
+                        for t in g.targets:
+                            # ignore unknowns
+                            if(t != GamrTarget.UNKNOWN and t != GamrTarget.SCALE and t != GamrTarget.MYSTERY_BLOCK):
+                                blocks.append(t.value)
+
+                if(len(blocks) > 0):
+                    LogCsv(outputFile, frameCount, blocks, f[2], f[1])
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gamrPath', nargs='?', default="F:\\Weights_Task\\Data\\GAMR\\CSV\\Group_05_merge_CM.csv")
+    #parser.add_argument('--annotationsFile', nargs='?', default="F:\\Weights_Task\\Data\\Pointing\\KeyFrameSelectionOutput\\Group_1-master\\point\\combined.csv")
+    parser.add_argument('--annotationsFile', nargs='?', default="F:\\Weights_Task\\Data\\Pointing\\GroundTruthFrames\\Group5.csv")
+    parser.add_argument('--outputFile', nargs='?', default="F:\\Weights_Task\\Data\\FactPostProcessing\\Gesture\\Group5.csv")
+    args = parser.parse_args()
+
+    create_gesture_input(args.gamrPath, args.annotationsFile, args.outputFile)
+
---
+++ wtd_annotations/postProcessGTConversion_utterance.py
@@ -0,0 +1,43 @@
+import argparse
+from utils import loadUtteranceFeatures
+import os
+import csv
+
+def initalizeCsv(path):
+    if os.path.exists(path):
+        os.remove(path)
+
+    with open(path, 'a', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["utterance_id", "frame_received", "speaker_id", "text", "start_frame", "stop_frame", "audio_file"])
+    return 
+
+def LogCsv(path, utterance_id, frame_received, speaker_id, text, start_frame, stop_frame, audio_file):
+    with open(path, 'a', newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow([utterance_id, frame_received, speaker_id, text, start_frame, stop_frame, audio_file])
+    return 
+
+
+def create_utterance_input(utterancePath, outputFile):
+    initalizeCsv(outputFile)
+
+    utteranceFeatures = loadUtteranceFeatures(utterancePath)
+    assert utteranceFeatures is not None
+
+    #for each ground truth utterance log the ASR values
+    count = 1
+    for u in utteranceFeatures:
+        print(u)
+        startFrame = int(float(u[1]) * 30)
+        endFrame = int(float(u[2]) * 30)
+        LogCsv(outputFile, count, endFrame, "Group", u[3], startFrame, endFrame, "")
+        count += 1
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--utterancePath', nargs='?', default="F:\\Weights_Task\\Data\\GAMR\\Utterances\\Group_01.csv")
+    parser.add_argument('--outputFile', nargs='?', default="F:\\Weights_Task\\Data\\FactPostProcessing\\Utterances\\Group_01.csv")
+    args = parser.parse_args()
+
+    create_utterance_input(args.utterancePath, args.outputFile)
