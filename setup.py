09b9ceed3c0edf1675a084e7123dcfd85d9574d6
Bhalla <brady03@colostate.edu>
2024-07-30 16:51:46 -0600

    change path in package setup

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ K4ABT_DIR = Path(r"C:\Program Files\Azure Kinect Body Tracking SDK")
 assert platform.system() == "Windows", "This package only works on Windows"
 
 # download json.hpp if needed
-json_dir = Path(".").resolve()
+json_dir = Path(".") / "azure_kinect_wrapper"
 if not os.path.exists(json_dir / "nlohmann/json.hpp"):
     os.makedirs(json_dir / "nlohmann", exist_ok=True)
     urllib.request.urlretrieve(
