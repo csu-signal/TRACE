567b2fea4f8405d072e28b7ac1ff96d55dbf45f8
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-08-07 15:07:10 -0600

    interface updates

--- mmdemo/base_feature.py
+++ mmdemo/base_feature.py
@@ -10,35 +10,32 @@ from mmdemo.base_interface import BaseInterface
 
 class BaseFeature(ABC):
     """
-    The base class all features in the demo
-    must implement.
+    The base class all features in the demo must implement.
     """
 
     def __init__(self) -> None:
         self._deps = []
         self._rev_deps = []
 
-        self._clean = False
-
     @final
-    def register_dependencies(self, deps: "list[BaseFeature]"):
+    def register_dependencies(
+        self, interfaces: list[Type[BaseInterface]], deps: "list[BaseFeature]"
+    ):
         """
         Add other features as dependencies which are required
         to be evaluated before this feature.
 
         Arguments:
+        interfaces -- a list of required dependency interface types
         deps -- a list of dependency features
         """
-        for d in deps:
+        for d, i_type in zip(deps, interfaces):
+            assert isinstance(
+                d.get_output_interface(), i_type
+            ), f"A dependency did not have output interface {i_type}"
+
             self._deps.append(d)
             d._rev_deps.append(self)
-
-    @final
-    def _mark_dirty(self):
-        if self.should_mark_dirty():
-            self._clean = False
-            for d in self._rev_deps:
-                d._mark_dirty()
 
     @classmethod
     @abstractmethod
@@ -50,14 +47,17 @@ class BaseFeature(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def get_output(self, *args: BaseInterface) -> BaseInterface:
+    def get_output(self, *args: BaseInterface) -> BaseInterface | None:
         """
         Return output of the feature. The return type must be
-        `self.get_output_interface()`.
+        `self.get_output_interface()` to provide new data and
+        `None` if there is no new data.
 
         Arguments:
         args -- list of output interfaces from dependencies in the order
-                they were registered
+                they were registered. Calling `.is_new()` on any of these
+                elements will return True if the argument has not been
+                sent before.
         """
         raise NotImplementedError
 
@@ -78,24 +78,5 @@ class BaseFeature(ABC):
         """
         Return True if the demo should exit. This will
         always return False if not overridden.
-        """
-        return False
-
-    def should_mark_dirty(self):
-        """
-        Return True if the feature should be marked as dirty
-        (meaning it needs to be re-evaluated). This will always
-        return True if not overridden and is called whenever a
-        dependency is marked as dirty.
-        """
-        return True
-
-    def has_external_input(self):
-        """
-        Return True if the feature should be marked as dirty
-        (meaning it needs to be re-evaluated) because of some
-        input not related to its dependencies. This will always
-        return False if not overridden as will be called
-        periodically during the demo.
         """
         return False
--- mmdemo/base_interface.py
+++ mmdemo/base_interface.py
@@ -2,9 +2,19 @@
 Base interface definition
 """
 
+from dataclasses import dataclass, field
 
+
+@dataclass
 class BaseInterface:
     """
-    Base class all output interfaces in
-    the demo must inherit from.
+    Base class all output interfaces in the demo must inherit from.
     """
+
+    _new: bool = field(default=True, init=False)
+
+    def is_new(self) -> bool:
+        """
+        Return True if this interface contains new data.
+        """
+        return self._new
