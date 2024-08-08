145c4039ace25a8ad083b5cd3699355cd14c7665
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-08-08 12:29:05 -0600

    snake case and reordering init to group by feature type

--- mmdemo/features/__init__.py
+++ mmdemo/features/__init__.py
@@ -1,23 +1,21 @@
 """
 Premade features
 """
+# from mmdemo.features.color.color_feature import Color
+# from mmdemo.features.depth.depth_feature import Depth
+# from mmdemo.features.body_tracking.body_tracking_feature import BodyTracking
 
 from mmdemo.features.asr.asr_feature import ASR
-from mmdemo.features.bodyTracking.body_tracking_feature import BodyTracking
-from mmdemo.features.cgTracking.cgt_feature import CommonGroundTracking
-from mmdemo.features.color.color_feature import Color
-
-# from mmdemo.features.denseParaphrasing.dense_paraphrase_feature import DenseParaphrasing
-from mmdemo.features.depth.depth_feature import Depth
-
-# from mmdemo.features.evaluation.evaluation_feature import Evaluation
+from mmdemo.features.common_ground.cgt_feature import CommonGroundTracking
+from mmdemo.features.dense_paraphrasing.dense_paraphrase_feature import (
+    DenseParaphrasing,
+)
+from mmdemo.features.evaluation.evaluation_feature import Evaluation
 from mmdemo.features.gaze.gaze_feature import Gaze
 from mmdemo.features.gesture.gesture_feature import Gesture
-
-# from mmdemo.features.gui.gui_feature import GUI
+from mmdemo.features.gui.gui_feature import GUI
 from mmdemo.features.move.move_feature import Move
 from mmdemo.features.objects.object_feature import Object
+from mmdemo.features.output_frames.output_frames_feature import OutputFrames
 from mmdemo.features.proposition.prop_feature import Proposition
-
-# from mmdemo.features.outputFrames.output_frames_feature import OutputFrames
-from mmdemo.features.selectedObjects.selected_objects_feature import SelectedObjects
+from mmdemo.features.selected_objects.selected_objects_feature import SelectedObjects
