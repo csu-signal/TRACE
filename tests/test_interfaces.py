78cca3bd8d2a7bfd6f8c745497b42496fdf7abae
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-08-08 13:41:11 -0600

    improved test example

--- tests/test_interfaces.py
+++ tests/test_interfaces.py
@@ -4,7 +4,16 @@ from mmdemo.base_interface import BaseInterface
 from mmdemo.interfaces import EmptyInterface
 
 
-@pytest.mark.parametrize("interface", [BaseInterface(), EmptyInterface()])
+@pytest.fixture(params=[(BaseInterface, ()), (EmptyInterface, ())])
+def interface(request):
+    cls, args = request.param
+    return cls(*args)
+
+
+def test_base_class(interface):
+    assert isinstance(interface, BaseInterface)
+
+
 def test_is_new(interface):
     assert interface.is_new()
     interface._new = False
