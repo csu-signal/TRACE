7c6c35b43c51b212867e73f5bb9ffb53fefcd73f
A-Ulkryxx <Ayoungren94@gmail.com>
2024-08-08 15:18:24 -0600

    merge color feat

--- mmdemo/features/color/color_feature.py
+++ mmdemo/features/color/color_feature.py
@@ -9,9 +9,9 @@ from mmdemo.interfaces import ColorImageInterface
 
 @final
 class Color(BaseFeature):
-    def __init__(self, *args):
-        super().__init__()
-        self.register_dependencies([], args)
+    @classmethod
+    def get_input_interfaces(cls):
+        return []
 
     @classmethod
     def get_output_interface(cls):
