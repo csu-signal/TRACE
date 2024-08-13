95af3d4768bf10c4eafbc55cdbc9b813a1361c0e
A-Ulkryxx <Ayoungren94@gmail.com>
2024-08-13 15:56:33 -0600

    Organized data classes to alphabetical ordering

--- mmdemo/interfaces/__init__.py
+++ mmdemo/interfaces/__init__.py
@@ -13,120 +13,6 @@ from mmdemo.interfaces.data import ObjectInfo2D, ObjectInfo3D, UtteranceInfo
 
 
 @dataclass
-class EmptyInterface(BaseInterface):
-    """
-    Output interface when the feature does not have any output
-    """
-
-
-class CameraCalibrationInterface(BaseInterface):
-    # TODO: brady add docstring
-    """
-    rotation
-    translation: np.ndarray
-    cameraMatrix: np.ndarray
-    distortion: np.ndarray
-    """
-    rotation: np.ndarray
-    translation: np.ndarray
-    cameraMatrix: np.ndarray
-    distortion: np.ndarray
-
-
-@dataclass
-class ColorImageInterface(BaseInterface):
-    """
-    frame -- image data with shape (h, w, 3)
-    """
-
-    frame_count: int
-    frame: np.ndarray
-
-
-@dataclass
-class DepthImageInterface(BaseInterface):
-    """
-    frame -- depth image with shape (h, w)
-    """
-
-    frame_count: int
-    frame: np.ndarray
-
-
-@dataclass
-class BodyTrackingInterface(BaseInterface):
-    """
-    bodies -- [{
-                    'body_id': unique identifier,
-                    'joint_positions': [xyz position],
-                    'join_orientation': [wxyz quaternion]
-                }, ...]
-    timestamp_usec -- timestamp in microseconds
-    """
-
-    bodies: list[dict[str, Any]]
-    timestamp_usec: int
-
-
-@dataclass
-class ObjectInterface2D(BaseInterface):
-    """
-    Object detector outputs
-
-    objects -- list of object locations and classes
-    """
-
-    objects: list[ObjectInfo2D]
-
-
-@dataclass
-class ObjectInterface3D(BaseInterface):
-    """
-    Object detector 3d locations
-
-    objects -- list of object locations and classes
-    """
-
-    objects: list[ObjectInfo3D]
-
-
-@dataclass
-class SelectedObjectsInterface(BaseInterface):
-    """
-    Which objects are selected by participants
-
-    objects -- [(object info, selected?), ...]
-    """
-
-    objects: list[tuple[ObjectInfo2D | ObjectInfo3D, bool]]
-
-
-# TODO: I assume these should be 2d for easier use with
-# with rgb-only demos. We could also have separate ones
-# for 2d/3d?
-
-
-@dataclass
-class Vectors2D(BaseInterface):
-    # TODO: Hannah change if needed
-    """
-    vectors -- list of numpy vectors with shape (2,)
-    """
-
-    vectors: list[np.ndarray]
-
-
-@dataclass
-class Vectors3D(BaseInterface):
-    # TODO: Hannah change if needed
-    """
-    vectors -- list of numpy vectors with shape (3,)
-    """
-
-    vectors: list[np.ndarray]
-
-
-@dataclass
 class _AudioChunkBase(BaseInterface):
     """Base interface for audio chunks"""
 
@@ -168,41 +54,42 @@ class AudioFileInterface(_AudioChunkBase):
 
 
 @dataclass
-class TranscriptionInterface(BaseInterface):
-    # TODO: brady figure out how to get frames
+class BodyTrackingInterface(BaseInterface):
     """
-    Transcribed utterances
-
-    info -- identifying data for the utterance
-    text -- text of the utterance
+    bodies -- [{
+                    'body_id': unique identifier,
+                    'joint_positions': [xyz position],
+                    'join_orientation': [wxyz quaternion]
+                }, ...]
+    timestamp_usec -- timestamp in microseconds
     """
 
-    speaker_id: str
-    start_time: float
-    end_time: float
-    text: str
+    bodies: list[dict[str, Any]]
+    timestamp_usec: int
+
+
+class CameraCalibrationInterface(BaseInterface):
+    # TODO: brady add docstring
+    """
+    rotation
+    translation: np.ndarray
+    cameraMatrix: np.ndarray
+    distortion: np.ndarray
+    """
+    rotation: np.ndarray
+    translation: np.ndarray
+    cameraMatrix: np.ndarray
+    distortion: np.ndarray
 
 
 @dataclass
-class PropositionInterface(BaseInterface):
-    # TODO: brady docstring
+class ColorImageInterface(BaseInterface):
     """
-    prop -- proposition as a string
+    frame -- image data with shape (h, w, 3)
     """
-    speaker_id: str
-    prop: str
 
-
-@dataclass
-class MoveInterface(BaseInterface):
-    # TODO: brady docstring
-    """
-    info -- identifying data for the utterance
-    move -- iterable containing some subset of
-            {"STATEMENT", "ACCEPT", "DOUBT"}
-    """
-    speaker_id: str
-    move: Iterable[str]
+    frame_count: int
+    frame: np.ndarray
 
 
 @dataclass
@@ -218,9 +105,122 @@ class CommonGroundInterface(BaseInterface):
 
 
 @dataclass
+class DepthImageInterface(BaseInterface):
+    """
+    frame -- depth image with shape (h, w)
+    """
+
+    frame_count: int
+    frame: np.ndarray
+
+
+@dataclass
+class EmptyInterface(BaseInterface):
+    """
+    Output interface when the feature does not have any output
+    """
+
+
+@dataclass
+class MoveInterface(BaseInterface):
+    # TODO: brady docstring
+    """
+    info -- identifying data for the utterance
+    move -- iterable containing some subset of
+            {"STATEMENT", "ACCEPT", "DOUBT"}
+    """
+    speaker_id: str
+    move: Iterable[str]
+
+
+@dataclass
+class ObjectInterface2D(BaseInterface):
+    """
+    Object detector outputs
+
+    objects -- list of object locations and classes
+    """
+
+    objects: list[ObjectInfo2D]
+
+
+@dataclass
+class ObjectInterface3D(BaseInterface):
+    """
+    Object detector 3d locations
+
+    objects -- list of object locations and classes
+    """
+
+    objects: list[ObjectInfo3D]
+
+
+@dataclass
 class PoseInterface(BaseInterface):
     """
     poses -- list of (body_id, "leaning in" / "leaning out")
     """
 
     poses: list[tuple[int, str]]
+
+
+@dataclass
+class PropositionInterface(BaseInterface):
+    # TODO: brady docstring
+    """
+    prop -- proposition as a string
+    """
+    speaker_id: str
+    prop: str
+
+
+@dataclass
+class SelectedObjectsInterface(BaseInterface):
+    """
+    Which objects are selected by participants
+
+    objects -- [(object info, selected?), ...]
+    """
+
+    objects: list[tuple[ObjectInfo2D | ObjectInfo3D, bool]]
+
+
+@dataclass
+class TranscriptionInterface(BaseInterface):
+    # TODO: brady figure out how to get frames
+    """
+    Transcribed utterances
+
+    info -- identifying data for the utterance
+    text -- text of the utterance
+    """
+
+    speaker_id: str
+    start_time: float
+    end_time: float
+    text: str
+
+
+# TODO: I assume these should be 2d for easier use with
+# with rgb-only demos. We could also have separate ones
+# for 2d/3d?
+
+
+@dataclass
+class Vectors2DInterface(BaseInterface):
+    # TODO: Hannah change if needed
+    """
+    vectors -- list of numpy vectors with shape (2,)
+    """
+
+    vectors: list[np.ndarray]
+
+
+@dataclass
+class Vectors3DInterface(BaseInterface):
+    # TODO: Hannah change if needed
+    """
+    vectors -- list of numpy vectors with shape (3,)
+    """
+
+    vectors: list[np.ndarray]
