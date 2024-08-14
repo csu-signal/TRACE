1cfdc78e882d9654db4f29f9e81ee15cd5b0e4c1
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-08-14 12:29:10 -0600

    revert object test changes

--- tests/features/test_object_feature.py
+++ tests/features/test_object_feature.py
@@ -6,6 +6,36 @@ from mmdemo.features.objects.object_feature import Object
 from mmdemo.interfaces import ColorImageInterface, ObjectInterface3D
 
 
+@final
+@pytest.mark.xfail
+def test_import():
+    """
+    Check that imports work
+    """
+    from mmdemo.interfaces import ColorImageInterface, ObjectInterface3D
+
+
+@final
+@pytest.mark.xfail
+def test_input_interfaces(objects: Object):
+    args = objects.get_input_interfaces()
+    assert len(args) == 1
+    assert isinstance(args, list)
+
+
+@pytest.mark.xfail
+def test_output_interface(objects: Object):
+    assert isinstance(objects.get_output_interface(), ObjectInterface3D)
+
+
 def test_output(objects: Object):
     output = objects.get_output()
     assert isinstance(output, ObjectInterface3D)
+
+
+objects = Object()
+
+test_import()
+test_input_interfaces(objects)
+test_output_interface(objects)
+test_output(objects)
