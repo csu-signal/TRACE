1a3284d9c1bcbe3f91d2e4266dffb91275a7f844
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-08-16 15:45:58 -0600

    initial cgt tests

--- tests/features/test_cgt_feature.py
+++ tests/features/test_cgt_feature.py
@@ -6,28 +6,43 @@ from mmdemo.interfaces import CommonGroundInterface, MoveInterface, PropositionI
 
 @pytest.fixture(scope="module")
 def cgt_feature():
+    """
+    Uses the same feature for all tests in this file,
+    so the order that tests are run will matter because an
+    internal state is stored
+    """
     cgt = CommonGroundTracking()
     cgt.initialize()
     yield cgt
     cgt.finalize()
 
 
+# TODO: See if Nikhil can write tests for these:
+#   - multiple moves at the same time
+#   - accepts / doubts with no previous statement
+#   - !=, >, <, >=, <=
 @pytest.mark.parametrize(
-    "prop, move, expected",
+    "prop, move, ebank, fbank",
     [
-        ("red = 10", "STATEMENT"),
-        ("10", "STATEMENT"),
-        ("red", "STATEMENT"),
-        ("green = 20", "STATEMENT"),
-        ("red > blue", "STATEMENT"),
-        ("red = blue", "STATEMENT"),
-        ("yellow != 40", "DOUBT"),
-        ("red != green", "DOUBT"),
-        ("I agree", "ACCEPT"),
+        ("red = 10", {"STATEMENT"}, {"red=10"}, set()),
+        ("red = 10", {"ACCEPT"}, set(), {"red=10"}),
+        ("blue = 10", {"STATEMENT"}, {"blue=10"}, {"red=10"}),
+        ("blue = 20", {"STATEMENT"}, {"blue=10", "blue=20"}, {"red=10"}),
+        ("blue = 20", {"ACCEPT"}, set(), {"red=10", "blue=20"}),
+        ("blue = 20", {"DOUBT"}, {"blue=20"}, {"red=10"}),
+        ("blue = 10", {"STATEMENT"}, {"blue=10", "blue=20"}, {"red=10"}),
+        ("blue = 10", {"ACCEPT"}, set(), {"red=10", "blue=10"}),
     ],
 )
-def test_cgt_feature(cgt_feature: CommonGroundTracking, input, expected):
+def test_cgt_feature(cgt_feature: CommonGroundTracking, prop, move, ebank, fbank):
     """
     Test that common ground tracking works correctly.
     """
-    output = cgt_feature.get_output()
+    output = cgt_feature.get_output(
+        MoveInterface(speaker_id="test", move=move),
+        PropositionInterface(speaker_id="test", prop=prop),
+    )
+    assert isinstance(output, CommonGroundInterface)
+
+    assert output.ebank == ebank
+    assert output.fbank == fbank
