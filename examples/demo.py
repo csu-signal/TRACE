16bdc6c6ed8bc3bc8888c3335bf2cf2f1f41906b
Brady Bhalla <34150846+bradybhalla@users.noreply.github.com>
2024-08-20 12:33:50 -0600

    demo ready to run

--- examples/demo.py
+++ examples/demo.py
@@ -1,48 +1,36 @@
 import pickle
 
-from mmdemo_azure_kinect import DeviceType, create_azure_kinect_features
-
 import mmdemo.features as fs
 from mmdemo.base_feature import BaseFeature
 from mmdemo.demo import Demo
+from mmdemo.features.common_ground.cgt_feature import CommonGroundTracking
+from mmdemo.features.move.move_feature import Move
+from mmdemo.features.proposition.prop_feature import Proposition
+from mmdemo.features.transcription.whisper_transcription_feature import (
+    WhisperTranscription,
+)
+from mmdemo.features.utterance.audio_input_features import MicAudio
+from mmdemo.features.utterance.vad_builder_feature import VADUtteranceBuilder
+
+# from mmdemo_azure_kinect import DeviceType, create_azure_kinect_features
 
 
 class PrintFeature(BaseFeature):
-    def initialize(self):
-        self.c = 0
-
     def get_output(self, *args):
-        if not all(i.is_new() for i in args):
-            return None
-
-        if self.c in [0, 197, 359, 1073]:
-            with open(f"frame{self.c:05}.pkl", "wb") as f:
-                pickle.dump(args, f)
-
-        self.c += 1
-    
-    def is_done(self) -> bool:
-        return self.c > 1100
+        print(args)
 
 
 if __name__ == "__main__":
-    color, depth, bt, calibration = create_azure_kinect_features(
-        DeviceType.PLAYBACK,
-        mkv_path=r"F:\brady_recording_tests\full_run_7_26\run03-master.mkv",
-        mkv_frame_rate=30,
-        playback_frame_rate=30,
-    )
+    mic = MicAudio(device_id=1)
 
-    # mic = fs.MicAudio(device_id=1)
-    #
-    # utterances = fs.VADUtteranceBuilder(mic)
-    #
-    # transcription = PrintFeature(fs.WhisperTranscription(utterances))
-    #
-    # props = PrintFeature(fs.Proposition(transcription))
-    #
-    # moves = PrintFeature(fs.Move(transcription, utterances))
-    #
-    # cgt = fs.CommonGroundTracking(moves, props)
+    utterances = VADUtteranceBuilder(mic)
 
-    Demo(targets=[PrintFeature(color, depth, bt, calibration)]).run()
+    transcription = WhisperTranscription(utterances)
+
+    props = Proposition(transcription)
+
+    moves = Move(transcription, utterances)
+
+    cgt = CommonGroundTracking(moves, props)
+
+    Demo(targets=[PrintFeature(transcription, props)]).run()
