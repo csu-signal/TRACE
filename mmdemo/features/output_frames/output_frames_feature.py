8fc1761b0f03111219dbd5d0260bb4943c2ca7a1
Hannah VAnderHoeven <hannahvanderh@gmail.com>
2024-08-20 21:35:44 -0600

    output frame feature and tests

--- mmdemo/features/output_frames/output_frames_feature.py
+++ mmdemo/features/output_frames/output_frames_feature.py
@@ -12,9 +12,27 @@ from mmdemo.interfaces import (  # FrameCountInterface,; GazeInterface,
     SelectedObjectsInterface
 )
 from mmdemo.utils.coordinates import _convert2D
+import re
 
 # import helpers
 # from mmdemo.features.proposition.helpers import ...
+
+#TODO add documentation 
+
+class Color():
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+colors = [
+        Color("red", (0, 0, 255)), 
+        Color("blue", (255, 0, 0)), 
+        Color("green", (19, 129, 51)), 
+        Color("purple", (128, 0, 128)), 
+        Color("yellow", (0, 215, 255))]
+
+fontScales = [1.5, 1.5, 0.75, 0.5, 0.5]
+fontThickness = [3, 3, 2, 2, 2]
 
 
 @final
@@ -57,9 +75,19 @@ class OutputFrames(BaseFeature[ColorImageInterface]):
         for cone in gesture.cones:
             self.projectVectorLines(cone, color.frame, calibration, True, False, False)
 
-        #TODO render objects
+        #render objects
+        for obj in objects.objects:
+            color = (0,255,0) if obj[1] == True else (0,0,255)
+            block = obj[0]
+            cv.rectangle(color.frame, 
+                    (int(block.p1[0]), int(block.p1[1])),
+                    (int(block.p2[0]), int(block.p2[1])),
+                    color=color,
+                    thickness=5)
 
-        #TODO render common ground
+        #render common ground
+        self.renderBanks(color.frame, 130, 260, "FBank", common.fbank)
+        self.renderBanks(color.frame, 130, 130, "EBank",  common.ebank)
 
         return color
 
@@ -103,4 +131,48 @@ class OutputFrames(BaseFeature[ColorImageInterface]):
 
             cv.line(frame, vertexPointUpZ, pointUpZ, color=ZColor, thickness=5)
             cv.line(frame, vertexPpointDownZ, pointDownZ, color=ZColor, thickness=5)
+
+    def getPropValues(self, propStrings, match):
+        label = []
+        for prop in propStrings:
+            prop_match = re.match(r'(' + match + r')\s*(=|<|>|!=)\s*(.*)', prop)
+            if prop_match:
+                block = prop_match[1]
+                relation = prop_match[2]
+                rhs = prop_match[3]
+                if(relation == '<' or relation == '>' or relation == '!='):
+                    label.append(relation + rhs)
+                else:
+                    label.append(rhs)
+        return label
+    
+    def renderBanks(self, frame, xSpace, yCord, bankLabel, bankValues):
+        blocks = len(colors) + 1
+        blockWidth = 112
+        blockHeight = 112
+
+        h,w,_ = frame.shape
+        start = w - (xSpace * blocks)
+        p2 = h - yCord
+        (tw, th), _ = cv.getTextSize(bankLabel, cv.FONT_HERSHEY_SIMPLEX, 1.5, 3)
+        labelCoords = (int(start) - int(tw / 4), (int(blockHeight / 2) + int(th / 2)) + p2)
+        cv.putText(frame, bankLabel, labelCoords, cv.FONT_HERSHEY_SIMPLEX, 1.5, (0,0,0), 3)
+
+        for i in range(1, blocks):
+            p1 = start + (xSpace * i)
+            color = colors[i - 1]
+            cv.rectangle(frame, 
+                (p1, p2), 
+                (p1 + blockWidth, p2 + blockHeight), 
+                color=color.color,
+                thickness=-1)
+            
+            labels = self.getPropValues(bankValues, color.name)
+            numberLabels = min(len(labels), 5)
+            if(numberLabels > 0):
+                for i, line in enumerate(labels):
+                    (tw, th), _ = cv.getTextSize(line, cv.FONT_HERSHEY_SIMPLEX, fontScales[numberLabels - 1], fontThickness[numberLabels -1])
+                    y = ((int(blockHeight / (numberLabels + 1)) + int(th / 3)) * (i + 1)) + p2
+                    x = (int(blockWidth / 2) - int(tw / 2)) + p1
+                    cv.putText(frame, line, (x, y), cv.FONT_HERSHEY_SIMPLEX, fontScales[numberLabels - 1], (0,0,0), fontThickness[numberLabels -1])
 
